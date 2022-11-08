4dad037cc46431510625960d36aa42827a7692de
DanielFrones <daniel.frones@gmail.com>
2022-11-08 13:36:30 +0100

    Add project files.

---
+++ Kunskapsprov/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Kunskapsprov.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
